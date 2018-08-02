217c9e093275c0ad6a950107cab445ea1128b8d3
nickfp1985 <nfpeddie@gmail.com>
2018-08-01 19:22:43 -0700

    displaying all inventory with no errors

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,46 +2,62 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 
 // create a connection to bamazonDB
-let connection = mysql.connection({
+let connection = mysql.createConnection({
     host: 'localhost',
-    port: 8080,
+    port: 3306,
     user: 'root',
     password: 'rootroot',
     database: 'bamazonDB'
 });
 
 // connect to server and database
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    startApp();
+    console.log('hola connection please');
+    return;
 })
 
 function displayProducts() {
-    connection.query("SELECT * FROM products", function(err, data) {
+    connection.query('SELECT * FROM products', 'utf-8', function (err, data) {
         if (err) throw err;
-
-        console.log('')
+        data.forEach(function (info) {
+            console.log(`
+               product:
+               ==============================
+               id: ${info.id}
+               name: ${info.product_name}
+               price: ${info.price}
+               quantity: ${info.stock_quantity}
+            `);
+        });
     })
 }
+
 
 // ask users what and how many they would like to buy
-function promptUsers() {
-    inquirer.prompt([
-        {
-            name: 'id',
-            type: 'input',
-            message: 'Enter the product ID that you would like to buy.',
-            validate: validateInput,
-            filter: Number
-        },
-        {
-            name: 'quantity',
-            type: 'input',
-            message: 'How many units would you like to buy?',
-            validate: validateInput,
-            filter: Number
-        }
-    ]).then(function(answer) {
-        
-    })
+// function promptUsers() {
+//     inquirer.prompt([
+//         {
+//             name: 'id',
+//             type: 'input',
+//             message: 'Enter the product ID that you would like to buy.',
+//             validate: validateInput,
+//             filter: Number
+//         },
+//         {
+//             name: 'quantity',
+//             type: 'input',
+//             message: 'How many units would you like to buy?',
+//             validate: validateInput,
+//             filter: Number
+//         }
+//     ]).then(function(answer) {
+
+//     })
+// }
+
+function start() {
+    displayProducts();
 }
+
+start();
